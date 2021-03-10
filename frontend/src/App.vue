7f5b18d5f54187bc692fe942897c51cf4c94fe2f
Elad Ab <abramovich.elad@gmail.com>
2021-03-10 12:23:50 +0200

    Frontend cleanup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,24 +9,39 @@
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+@import '~normalize.css';
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+}
+a {
+  text-decoration: none;
+}
+
+input {
+  /* Remove Safari styling */
+  border-radius: 0;
+  -webkit-appearance: none;
+}
+
+:root {
+  --primary-color: #f1cc03;
+  --text-color-primary: #2e2f2f;
+  --text-color-links: #6298e7;
+  --borders-color-primary: #324438;
+
+  --header-height: 65px;
+  --footer-height: 80px;
+  --content-min-height: calc(
+    100vh - var(--header-height) - var(--footer-height)
+  );
+}
+
+body {
+  font-family: 'Open Sans', 'Helvetica Neue', 'Arial', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ frontend/src/pages/HomePage/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 :class="styles.title">Hello Home</h1>
+</template>
+
+<script>
+import homeStyles from './HomePage.module.css';
+export default {
+  computed: {
+    styles() {
+      return homeStyles;
+    },
+  },
+};
+</script>
