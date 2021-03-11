81f97080ef975c83dbab047b1b1a789a438fef0e
Elad Ab <abramovich.elad@gmail.com>
2021-03-11 10:04:57 +0200

    Fixed header items alignment

--- frontend/src/components/App/AppNavBar/AppNavBar.vue
+++ frontend/src/components/App/AppNavBar/AppNavBar.vue
@@ -3,19 +3,19 @@
     <router-link to="/">
       <p :class="styles.logo">My Books List</p>
     </router-link>
-    <ul :class="styles.navList">
+    <ul :class="`${styles.navList} ${styles.alignCenter}`">
       <li :class="styles.navItem">
         <router-link to="/browse">Browse Books</router-link>
       </li>
     </ul>
-    <ul :class="`${styles.navList} ${styles.fixRight}`">
+    <ul :class="`${styles.navList} ${styles.alignRight}`">
       <li :class="styles.navItem">
         <router-link to="/register">
           <strong>Login/Rgister</strong>
         </router-link>
       </li>
     </ul>
-    <div :class="`${styles.hamburgerIcon} ${styles.fixRight}`">
+    <div :class="`${styles.hamburgerIcon} ${styles.alignRight}`">
       <span :class="styles.line1"></span>
       <span :class="styles.line2"></span>
       <span :class="styles.line3"></span>
