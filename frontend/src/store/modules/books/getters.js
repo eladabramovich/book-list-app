bf54ce51addd798d16af98c261fccb18d1446d4a
Elad Ab <abramovich.elad@gmail.com>
2021-03-25 15:51:39 +0200

    Added sorting to book reviews

--- frontend/src/store/modules/books/getters.js
+++ frontend/src/store/modules/books/getters.js
@@ -3,6 +3,11 @@ export default {
     return state.selectedBook;
   },
   bookReviews(state) {
-    return state.bookReviews;
+    return state.bookReviews.sort((a, b) => {
+      let timeA = new Date(a.createdAt).getTime();
+      let timeB = new Date(b.createdAt).getTime();
+
+      return timeB - timeA;
+    });
   },
 };
