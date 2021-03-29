5db24d47164cd285a7a3e2c2d41bf46f87a4c5ff
Elad Ab <abramovich.elad@gmail.com>
2021-03-29 10:47:31 +0300

    Added status code check

--- backend/middlewares/errorMiddlewares.js
+++ backend/middlewares/errorMiddlewares.js
@@ -5,16 +5,18 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  console.log('in error handler');
+  let statusCode;
   let message = '';
 
   if (err.code === 11000) {
     message = 'Unique Field';
+    statusCode = 400;
   } else {
     message = err.message;
   }
 
-  res.status(statusCode);
+  res.status(statusCode || (res.statusCode === 200 ? 500 : res.statusCode));
   res.json({
     message,
     stack: process.env.NODE_ENV === 'development' ? err.stack : null,
