598bb77bc55bc91407cad3e98a743899fda61e87
Elad Ab <abramovich.elad@gmail.com>
2021-03-29 09:46:54 +0300

    Added error message case

--- backend/middlewares/errorMiddlewares.js
+++ backend/middlewares/errorMiddlewares.js
@@ -6,14 +6,22 @@ const notFound = (req, res, next) => {
 
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = '';
+
+  if (err.code === 11000) {
+    message = 'Unique Field';
+  } else {
+    message = err.message;
+  }
+
   res.status(statusCode);
   res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'development' ? err.stack : null
+    message,
+    stack: process.env.NODE_ENV === 'development' ? err.stack : null,
   });
 };
 
 module.exports = {
   notFound,
-  errorHandler
+  errorHandler,
 };
