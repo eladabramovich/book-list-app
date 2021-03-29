cd794a8422e9162cb324f57e34634ee29f4d1736
Elad Ab <abramovich.elad@gmail.com>
2021-03-29 14:09:34 +0300

    Added auth middlewares

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/User');
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next();
+    } catch (err) {
+      console.error(err);
+      res.status(401);
+      throw new Error('Not authorizied, token failed');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorizied, no token');
+  }
+});
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorizied, not admin user');
+  }
+};
+
+module.exports = { protect, isAdmin };
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const advancedResults = require('../middlewares/advancedResults');
+const { protect, isAdmin } = require('../middlewares/authMiddleware');
 const Book = require('../models/Book');
 const {
   getAllBooks,
@@ -8,7 +9,10 @@ const {
   addBook,
 } = require('../controllers/bookController');
 
-router.route('/').get(advancedResults(Book), getAllBooks).post(addBook);
+router
+  .route('/')
+  .get(advancedResults(Book), getAllBooks)
+  .post(protect, isAdmin, addBook);
 router.route('/:id').get(getSingleBook);
 
 module.exports = router;
